3c0eb211e2067db9d9643d4487d0ac122492d17c
sali-k <saki.kimura78@gmail.com>
2020-09-28 15:54:11 +0900

    システムスペックの設定

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.0'
+  gem 'rails-controller-testing'
 end
 
 group :development do
@@ -56,8 +57,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'pry-rails'
-
 group :production do
   gem 'rails_12factor'
 end
+
+gem 'pry-rails'
+gem 'devise' 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,14 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+require 'capybara/rspec'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
