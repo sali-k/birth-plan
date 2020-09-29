8aa17c9863ad1507a0d3c711c8868f06c5894373
sali-k <saki.kimura78@gmail.com>
2020-09-29 15:00:56 +0900

    トップページのビュー変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def full_title(page_title = '')
     base_title = 'バースプラン'
-    if page_title.blank?
+    if page_title.empty?
       base_title
     else
-      "#{page_title} - #{base_title}"
+      page_title + " | " + base_title
     end
   end
 end
