4acb6cf44adb93076e0f7f3bf66910515f7507ed
sali-k <saki.kimura78@gmail.com>
2020-09-30 15:19:26 +0900

    ユーザー情報の編集と閲覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,20 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = current_user
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :introduction, :text, :relationship, :birth_date)
+    params.require(:user).permit(:name, :email, :introduction, :relationship, :birth_date)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+  validates :introduction, length: { maximum: 200 }
 
   validates :email, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@を含むアドレスを入力してください' }
 
