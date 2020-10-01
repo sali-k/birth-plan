a300c1c99f0140a070a6b30c833faf3e3d4bdcda
sali-k <saki.kimura78@gmail.com>
2020-10-01 22:48:35 +0900

    UserモデルRSpecテスト終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 10 }
   validates :introduction, length: { maximum: 200 }
+  validates :relationship, length: { maximum: 20 }
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は@と.を含むアドレスを入力してください' }
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて、8文字以上で設定してください', on: :create
---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :user do
+    name     { '太郎' }
+    email    { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 8)
+    password {password}
+    password_confirmation {password}
+    #password              { 'a12345678' }
+    #password_confirmation { password }
+    introduction          { 'はじめまして' }
+    relationship          { '夫' }
+  end
+end
