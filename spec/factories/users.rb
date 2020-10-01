2f265e8099e1b737bf4eaa020f45eb4d57453aee
sali-k <saki.kimura78@gmail.com>
2020-10-01 22:50:04 +0900

    ルボコップすみ

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,10 @@ FactoryBot.define do
     name     { '太郎' }
     email    { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 8)
-    password {password}
+    password { password}
     password_confirmation {password}
-    #password              { 'a12345678' }
-    #password_confirmation { password }
+    # password              { 'a12345678' }
+    # password_confirmation { password }
     introduction          { 'はじめまして' }
     relationship          { '夫' }
   end
