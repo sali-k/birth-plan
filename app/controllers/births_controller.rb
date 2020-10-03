4e22da5357d5b7f31ac76b0a5d53e13fd138df2c
sali-k <saki.kimura78@gmail.com>
2020-10-03 16:12:19 +0900

    バースプラン show

--- app/controllers/births_controller.rb
+++ app/controllers/births_controller.rb
@@ -1,6 +1,6 @@
 class BirthsController < ApplicationController
   def index
-    @births = Birth.all
+    @births = Birth.all.order("created_at DESC")
   end
 
   def new
@@ -8,11 +8,20 @@ class BirthsController < ApplicationController
   end
 
   def create
-    Birth.create(birth_params)
+    @birth = Birth.new(birth_params)
+    if @birth.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @birth = Birth.find(params[:id])
   end
 
   private
   def birth_params
-    params.require(:birth).permit(:user, :plan_name, :description, :hope_id, :attend_id, :contraction, :childbirth, :postpartum, :memo)
+    params.require(:birth).permit(:plan_name, :description, :hope_id, :attend_id, :contraction, :childbirth, :postpartum, :memo).merge(user_id: current_user.id)
   end
 end
