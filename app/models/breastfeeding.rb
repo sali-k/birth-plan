4285d8d5a986067887163e100768bd4629fb1016
sali-k <saki.kimura78@gmail.com>
2020-10-03 19:59:47 +0900

    バースプランの新規作成修正

--- app/models/breastfeeding.rb
+++ app/models/breastfeeding.rb
@@ -3,7 +3,7 @@ class Breastfeeding < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: '完全母乳' },
     { id: 2, name: '母乳とミルクの両方' },
-    { id: 2, name: 'ミルクのみ' },
-    { id: 2, name: '特に希望なし' },
+    { id: 3, name: 'ミルクのみ' },
+    { id: 4, name: '特に希望なし' },
   ]
 end
