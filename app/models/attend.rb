f3df45ac1dc51b86b79bf6161b471555c33808a5
sali-k <saki.kimura78@gmail.com>
2020-10-03 19:25:28 +0900

    新規バースプラン 作成

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -1,6 +1,6 @@
 class Attend < ActiveHash::Base
   self.data = [
-    { id: 0, name: '--' },
+    { id: 0, name: '---' },
     { id: 1, name: '夫' },
     { id: 2, name: '実母' },
     { id: 3, name: 'その他' },
--- app/models/hope.rb
+++ app/models/hope.rb
@@ -1,6 +1,6 @@
 class Hope < ActiveHash::Base
   self.data = [
-    { id: 0, name: '--' },
+    { id: 0, name: '---' },
     { id: 1, name: '希望する' },
     { id: 2, name: '希望しない' },
   ]
