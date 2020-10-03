6da925693de61448fb19342d45c1a1ef833aac9b
sali-k <saki.kimura78@gmail.com>
2020-10-03 11:38:16 +0900

    バースプラン create

--- app/controllers/births_controller.rb
+++ app/controllers/births_controller.rb
@@ -7,4 +7,12 @@ class BirthsController < ApplicationController
     @birth = Birth.new
   end
 
+  def create
+    Birth.create(birth_params)
+  end
+
+  private
+  def birth_params
+    params.require(:birth).permit(:user, :plan_name, :description, :hope_id, :attend_id, :contraction, :childbirth, :postpartum, :memo)
+  end
 end
--- app/models/birth.rb
+++ app/models/birth.rb
@@ -1,3 +1,9 @@
 class Birth < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :plan_name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :hope_id, numericality: { other_than: 0, message: 'を選択してください' }
+    validates :attend_id, numericality: { other_than: 0, message: 'を選択してください' }
 end
